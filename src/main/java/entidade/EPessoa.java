b9e1ea7dcd0e5ff28fc773bc9176d3c6c5ad247e
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-07-27 22:46:18 -0300

    Adiconado calculo dos impostos

--- src/main/java/entidade/EPessoa.java
+++ src/main/java/entidade/EPessoa.java
@@ -11,6 +11,8 @@ public class EPessoa implements Serializable{
 	
 	private String nome;
 	
+	private String sexo;
+	
 	private String email;
 	
 	private String observacao;
@@ -19,9 +21,94 @@ public class EPessoa implements Serializable{
 	
 	private Date data_nasc;
 	
-	private double salario;
+	private double salarioBruto;
+	
+	private double salarioLiquido;
+	
+	private double descInss;
+	
+	private double descIr;
 	
 	private String tipoPessoa;
+	
+		
+	
+	public double getSalarioLiquido() {
+		return salarioLiquido;
+	}
+
+	public void setSalarioLiquido(double salarioLiquido) {
+		this.salarioLiquido = salarioLiquido;
+	}
+
+	private double calculaDescInss() {
+		double desconto =  0.11 / this.salarioBruto;
+		double salarioDescontado = this.salarioBruto - desconto;
+		this.descInss = desconto;
+		return salarioDescontado;
+	}
+	
+	public double calculaDescsIr() {//Deve ser chamado para calcular todo o desconto
+		double salarioDesc =  this.calculaDescInss();
+		double desconto;
+		double total;
+
+		if(this.salarioBruto <= 1903.98) {
+			this.descIr = 0;
+			return salarioDesc;
+		}else if(this.salarioBruto >= 1903.99 && this.salarioBruto <= 2826.65) {
+			desconto = 0.075 / this.salarioBruto;
+			total = salarioDesc - desconto;
+			this.descIr = desconto;
+			
+			return total;
+
+		}else if(this.salarioBruto >= 2826.66 && this.salarioBruto <= 3751.05) {
+			desconto = 0.15 / this.salarioBruto;
+			total = salarioDesc - desconto;
+			this.descIr = desconto;
+
+			return total;
+
+		}else if(this.salarioBruto >= 3751.06 && this.salarioBruto < 4664.68) {
+			desconto = 0.225 / this.salarioBruto;
+			total = salarioDesc - desconto;
+			this.descIr = desconto;
+
+			return total;
+
+		}else{
+			desconto = 0.225 / this.salarioBruto;
+			total = salarioDesc - desconto;
+			this.descIr = desconto;
+
+			return total;
+		}
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public double getDescInss() {
+		return descInss;
+	}
+
+	public void setDescInss(double descInss) {
+		this.descInss = descInss;
+	}
+
+	public double getDescIr() {
+		return descIr;
+	}
+
+	public void setDescIr(double descIr) {
+		this.descIr = descIr;
+	}
 
 	public String getCpf() {
 		return cpf;
@@ -71,12 +158,12 @@ public class EPessoa implements Serializable{
 		this.data_nasc = data_nasc;
 	}
 
-	public double getSalario() {
-		return salario;
+	public double getSalarioBruto() {
+		return salarioBruto;
 	}
 
-	public void setSalario(double salario) {
-		this.salario = salario;
+	public void setSalarioBruto(double salario) {
+		this.salarioBruto = salario;
 	}
 
 	public String getTipoPessoa() {
